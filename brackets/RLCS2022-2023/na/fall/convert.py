5aaac69634f25b97d742e1daf86e47ed7445dc2d
nwright <nwright>
2022-10-19 13:11:09 -0400

    script to convert from old format to new

---
+++ brackets/RLCS2022-2023/na/fall/convert.py
@@ -0,0 +1,10 @@
+import csv
+
+with open('regional1.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        name = row[1][1:].replace(' ', '-')
+        f = open(name+'.json', "w")
+        f.write("{\"team\": \"" + row[0] +"\", \"url\": \""+ row[1][1:]+ "\"}")
+        f.close()
+        break
