868cfe057a9ab2c2b4bf84d9489815f161f4172a
nwright <nwright>
2022-10-19 13:24:01 -0400

    fixed conversion script

--- brackets/RLCS2022-2023/na/fall/convert.py
+++ brackets/RLCS2022-2023/na/fall/convert.py
@@ -5,5 +5,5 @@ with open('regional1.csv', newline='') as csvfile:
     for row in reader:
         name = row[1][1:].replace(' ', '-')
         f = open(name+'.json', "w")
-        f.write("{\"team\": \"" + row[0] +"\", \"url\": \""+ row[1][1:]+ "\"}")
+        f.write("{\"team\": \"" + row[1][1:] +"\", \"url\": \""+ row[0]+ "\"}")
         f.close()
